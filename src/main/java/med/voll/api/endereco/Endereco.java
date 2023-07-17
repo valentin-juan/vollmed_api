f413f7de549e1320b8727b8d9e3d17283ab6fba4
juanvalentin <valentin.juan05@gmail.com>
2023-07-17 17:08:09 -0300

    first commit

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,34 @@
+package med.voll.api.endereco;
+
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.uf = endereco.uf();
+        this.cidade = endereco.cidade();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+    }
+}
